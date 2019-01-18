c8d56d79afbbdb1cbc7d42af5d279bd29dadb22b
pankona <yosuke.akatsuka@gmail.com>
2019-01-18 14:02:09 +0900

    work without GITHUB_API_TOKEN

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"net/http"
 	"os"
 
 	"github.com/google/go-github/v21/github"
@@ -11,17 +12,17 @@ import (
 
 func main() {
 	token := os.Getenv("GITHUB_API_TOKEN")
-	if token == "" {
-		fmt.Println("Please specify GitHub API Token to GITHUB_API_TOKEN environment variable.")
-		os.Exit(1)
+
+	var tc *http.Client
+	if token != "" {
+		ts := oauth2.StaticTokenSource(
+			&oauth2.Token{AccessToken: token},
+		)
+		tc = oauth2.NewClient(oauth2.NoContext, ts)
 	}
 
-	ts := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: token},
-	)
-	tc := oauth2.NewClient(oauth2.NoContext, ts)
-
 	c := github.NewClient(tc)
+
 	_, resp, err := c.Activity.ListEventsPerformedByUser(
 		context.Background(), "pankona", true, nil)
 	if err != nil {
