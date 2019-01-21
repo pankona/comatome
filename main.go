b33ef2e37a1f1a3e7b8455bc8d8b0ca2e62773e2
pankona <yosuke.akatsuka@gmail.com>
2019-01-21 15:32:52 +0900

    strict by email (but sometimes fails to fetch result)

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"sort"
 
 	"github.com/google/go-github/v21/github"
 	"golang.org/x/oauth2"
@@ -23,27 +24,58 @@ func main() {
 
 	c := github.NewClient(tc)
 
-	page := 1
-	for {
-		result, resp, err := c.Search.Repositories(
-			context.Background(),
-			"user:pankona+created:2019-01-01..2019-02-01",
-			&github.SearchOptions{
-				ListOptions: github.ListOptions{
-					Page: page,
-				}})
-		if err != nil {
-			panic(err)
-		}
+	emails := []string{
+		"yosuke.akatsuka@gmail.com",
+		"yosuke.akatsuka@access-company.com",
+	}
 
-		page = resp.NextPage
+	m := make(map[string]int)
 
-		for _, v := range result.Repositories {
-			fmt.Printf("%s: createdAt: %v\n", *v.Name, *v.CreatedAt)
-		}
+	for _, email := range emails {
+		page := 1
+		for {
+			result, resp, err := c.Search.Commits(
+				context.Background(),
+				fmt.Sprintf("author-email:%s+author-date:2019-01-01..2019-02-01", email),
+				&github.SearchOptions{
+					ListOptions: github.ListOptions{
+						PerPage: 100,
+						Page:    page,
+					}})
+			if err != nil {
+				panic(err)
+			}
 
-		if resp.NextPage == 0 {
-			break
+			page = resp.NextPage
+
+			fmt.Printf("len: %d\n", len(result.Commits))
+			if *result.IncompleteResults {
+				panic("incomplete result is true")
+			}
+
+			for _, v := range result.Commits {
+				m[*v.Repository.FullName] += 1
+			}
+
+			if resp.NextPage == 0 {
+				break
+			}
 		}
 	}
+
+	keys := make([]string, len(m))
+	index := 0
+	for k, _ := range m {
+		keys[index] = k
+		index++
+	}
+	sort.Strings(keys)
+
+	total := 0
+	fmt.Println("commits on 2019-01-01..2019-02-01")
+	for _, v := range keys {
+		fmt.Printf("%3d commits on %s\n", m[v], v)
+		total += m[v]
+	}
+	fmt.Printf("total %d commits on this month\n", total)
 }
