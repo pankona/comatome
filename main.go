3bbfc527be20c6d7fcf9836a44a7c5b098e8f0ab
pankona <yosuke.akatsuka@gmail.com>
2019-01-25 16:24:43 +0900

    separate files

--- main.go
+++ main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"context"
 	"flag"
 	"fmt"
 	"os"
-
-	"github.com/google/go-github/v21/github"
 )
 
 func main() {
@@ -37,40 +34,4 @@ func createdRepos(c *Client, fromto string) {
 		panic(err)
 	}
 	showCreatedRepos(r)
-}
-
-func QueryCreatedRepos(c *Client, fromto string) ([]string, error) {
-	page := 1
-	createdRepos := make([]string, 0)
-	for {
-		result, resp, err := c.Search.Repositories(
-			context.Background(),
-			fmt.Sprintf("user:pankona+created:%s", fromto),
-			&github.SearchOptions{
-				ListOptions: github.ListOptions{
-					PerPage: 100,
-					Page:    page,
-				}})
-		if err != nil {
-			panic(err)
-		}
-
-		page = resp.NextPage
-
-		for _, v := range result.Repositories {
-			createdRepos = append(createdRepos, *v.FullName)
-		}
-
-		if resp.NextPage == 0 {
-			break
-		}
-	}
-	return createdRepos, nil
-}
-
-func showCreatedRepos(repos []string) {
-	for _, v := range repos {
-		fmt.Printf("%s\n", v)
-	}
-	fmt.Printf("%d repositories created\n", len(repos))
 }
---
+++ repos.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/google/go-github/v21/github"
+)
+
+func QueryCreatedRepos(c *Client, fromto string) ([]string, error) {
+	page := 1
+	createdRepos := make([]string, 0)
+	for {
+		result, resp, err := c.Search.Repositories(
+			context.Background(),
+			fmt.Sprintf("user:pankona+created:%s", fromto),
+			&github.SearchOptions{
+				ListOptions: github.ListOptions{
+					PerPage: 100,
+					Page:    page,
+				}})
+		if err != nil {
+			panic(err)
+		}
+
+		page = resp.NextPage
+
+		for _, v := range result.Repositories {
+			createdRepos = append(createdRepos, *v.FullName)
+		}
+
+		if resp.NextPage == 0 {
+			break
+		}
+	}
+	return createdRepos, nil
+}
+
+func showCreatedRepos(repos []string) {
+	for _, v := range repos {
+		fmt.Printf("%s\n", v)
+	}
+	fmt.Printf("%d repositories created\n", len(repos))
+}
