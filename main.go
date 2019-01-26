826600266d234b5c2eb2229a07c4a5ef7a1866b2
pankona <yosuke.akatsuka@gmail.com>
2019-01-26 11:52:33 +0900

    support to show opened pull requests

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"os"
+	"sort"
+
+	"github.com/google/go-github/v21/github"
 )
 
 func main() {
@@ -18,20 +22,76 @@ func main() {
 	commits(c, *fromto)
 	fmt.Println()
 	createdRepos(c, *fromto)
+	fmt.Println()
+	openedPullRequests(c, *fromto)
 }
 
 func commits(c *Client, fromto string) {
-	m, err := QueryCommitsPerRepo(c, fromto)
+	results, err := QueryCommitsPerRepo(c, fromto)
 	if err != nil {
 		panic(err)
 	}
-	ShowCommitsPerRepo(m)
+	ShowCommitsPerRepo(results)
 }
 
 func createdRepos(c *Client, fromto string) {
-	r, err := QueryCreatedRepos(c, fromto)
+	results, err := QueryCreatedRepos(c, fromto)
 	if err != nil {
 		panic(err)
 	}
-	showCreatedRepos(r)
+	showCreatedRepos(results)
+}
+
+func openedPullRequests(c *Client, fromto string) {
+	results, err := QueryOpenedPullRequests(c, fromto)
+	if err != nil {
+		panic(err)
+	}
+	ShowOpenedPullRequests(results)
+}
+
+func QueryOpenedPullRequests(c *Client, fromto string) (map[string]int, error) {
+	page := 1
+	pulls := make(map[string]int)
+	for {
+		result, resp, err := c.Search.Issues(
+			context.Background(),
+			fmt.Sprintf("type:pr+author:pankona+created:%s", fromto),
+			&github.SearchOptions{
+				ListOptions: github.ListOptions{
+					PerPage: 100,
+					Page:    page,
+				}})
+		if err != nil {
+			panic(err)
+		}
+
+		page = resp.NextPage
+
+		for _, v := range result.Issues {
+			pulls[*v.RepositoryURL] += 1
+		}
+
+		if resp.NextPage == 0 {
+			break
+		}
+	}
+	return pulls, nil
+}
+
+func ShowOpenedPullRequests(pulls map[string]int) {
+	keys := make([]string, len(pulls))
+	index := 0
+	for k, _ := range pulls {
+		keys[index] = k
+		index++
+	}
+	sort.Strings(keys)
+
+	total := 0
+	for _, v := range keys {
+		fmt.Printf("%d\t%s\n", pulls[v], v)
+		total += pulls[v]
+	}
+	fmt.Printf("%d pull requests opened\n", total)
 }
