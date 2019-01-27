5df87da2fb3d9ec661543e7f5cd32a955f9ec88c
pankona <yosuke.akatsuka@gmail.com>
2019-01-27 11:53:36 +0900

    refactoring. separate files

---
+++ pulls.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"sort"
+
+	"github.com/google/go-github/v21/github"
+)
+
+func QueryOpenedPullRequests(c *Client, fromto string) (map[string]int, error) {
+	page := 1
+	pulls := make(map[string]int)
+	for {
+		result, resp, err := c.Search.Issues(
+			context.Background(),
+			fmt.Sprintf("type:pr+author:pankona+created:%s", fromto),
+			&github.SearchOptions{
+				ListOptions: github.ListOptions{
+					PerPage: 100,
+					Page:    page,
+				}})
+		if err != nil {
+			panic(err)
+		}
+
+		page = resp.NextPage
+
+		for _, v := range result.Issues {
+			pulls[*v.RepositoryURL] += 1
+		}
+
+		if resp.NextPage == 0 {
+			break
+		}
+	}
+	return pulls, nil
+}
+
+func ShowOpenedPullRequests(pulls map[string]int) {
+	keys := make([]string, len(pulls))
+	index := 0
+	for k, _ := range pulls {
+		keys[index] = k
+		index++
+	}
+	sort.Strings(keys)
+
+	total := 0
+	for _, v := range keys {
+		fmt.Printf("%d\t%s\n", pulls[v], v)
+		total += pulls[v]
+	}
+	fmt.Printf("%d pull requests opened\n", total)
+}
